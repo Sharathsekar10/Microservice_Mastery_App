d5ae2dc9ee9ec704d18ba8c372381c770026baec
Sharathsekar10 <43611007+Sharathsekar10@users.noreply.github.com>
2026-08-28 11:36:51 +0530

    Block B Day 4 and  5 added Micro Service Projects (#3)
    * Added Order Service Project
    
    * Added Notification Service Project
    
    * Added Inventory service Project
    
    * Added the launchSettings.json for all the service Project
    
    * Delete NotificationService/NotificationService.csproj.user
    
    * Delete OrderService/OrderService.csproj.user
    
    * Delete InventoryService/InventoryService.csproj.user

---
+++ InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly Dictionary<int, int> productQuantity = new Dictionary<int, int>()
+        {
+            {1,5},
+            { 2,10 }
+        };
+
+
+        [HttpGet("health")]
+        public async Task<IActionResult> GetHealth()
+        {
+            return Ok(new { StatusCode = 200, Message = "Inventory Service is healthy" });
+        }
+
+        [HttpGet("products")]
+        public Task<IActionResult> GetProducts(int productId)
+        {
+            try
+            {
+                
+                IActionResult result;
+                if(productQuantity.TryGetValue(productId,out var quantites))
+                {
+                    result = Ok(new { ProductId = productId, Stock = quantites, StatusCode = 200 });
+                }
+                else
+                {
+                    result = NotFound(new { StatusCode = 404, Message = "Product not found" });
+                }
+
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult<IActionResult>(Problem(detail: ex.Message, statusCode: 500));
+            }
+        }
+    }
+}
