d10a96d158ee87401be1671c8d5e34396163af85
sharathsekar <sharathsekar10@gmail.com>
2026-08-28 10:35:11 +0530

    Added Notification Service Project

---
+++ NotificationService/Controllers/NotificationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        [HttpGet("health")]
+        public async Task<IActionResult> GetHealth()
+        {
+            return Ok(new { StatusCode = 200, Message = "Notification Service is healthy" });
+        }
+    }
+}
