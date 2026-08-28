a9595ce27c974a5ff8fc81e324626805df548cb6
Sharathsekar10 <43611007+Sharathsekar10@users.noreply.github.com>
2026-08-28 10:25:39 +0530

    Azure Fundamentals
    Added Basic Blob controller  and ContainerService (#2)
    Create Azure Storage account, App sevice and Service bus Namespace using Bicep

---
+++ Microservice_Mastery_App/Interface/IContainerService.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Microservice_Mastery_App.Interface
+{
+    public interface IContainerService
+    {
+        Task<BlobContainerClient> CreateContainerIfNotExistAsync();
+    }
+}
